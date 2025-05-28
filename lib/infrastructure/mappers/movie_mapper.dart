4db55c3ca3d4f8a96cd594116369c7bcd749570b
Yunier <yunierab88@gmail.com>
2025-05-28 10:18:24 -0300

    Se agrega mapper

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,29 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDbToEntity(MovieMovieDB movieDb) {
+    return Movie(
+      adult: movieDb.adult,
+      backdropPath:
+          (movieDb.backdropPath != "")
+              ? "https://image.tmdb.org/t/p/w500${movieDb.backdropPath}"
+              : 'https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+      genreIds: movieDb.genreIds.map((e) => e.toString()).toList(),
+      id: movieDb.id,
+      originalLanguage: movieDb.originalLanguage,
+      originalTitle: movieDb.originalTitle,
+      overview: movieDb.overview,
+      popularity: movieDb.popularity,
+      posterPath:
+          (movieDb.posterPath != "")
+              ? "https://image.tmdb.org/t/p/w500${movieDb.posterPath}"
+              : 'https://ih1.redbubble.net/image.4905811447.8675/flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+      releaseDate: movieDb.releaseDate,
+      title: movieDb.title,
+      video: movieDb.video,
+      voteAverage: movieDb.voteAverage,
+      voteCount: movieDb.voteCount,
+    );
+  }
+}
