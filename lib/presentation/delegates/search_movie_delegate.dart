44eba1c519fbee165a267dcc6e288db5b31db83f
Yunier <yunierab88@gmail.com>
2025-06-11 09:35:48 -0300

    Se trabaja en los iconos del cuadro de busqueda y sus funcionalidades

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,18 +1,40 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
   //Se sobreescribe el metodo searchFieldLabel de la clase SearchDelegate para cambiar el texto de la busqueda
   @override
-  String? get searchFieldLabel => "Buscar pelicula";
+  String? get searchFieldLabel => "Buscar película";
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [Text("BuildActions")];
+    return [
+      if (query.isNotEmpty)
+        FadeIn(
+          duration: Duration(microseconds: 200),
+          child: IconButton(
+            onPressed: () {
+              query =
+                  ""; //query es una variable que se utiliza para almacenar el texto de la busqueda y me la ofrece la clase SearchDelegate
+            },
+            icon: Icon(Icons.clear),
+          ),
+        ),
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return Text("BuildLeading");
+    return IconButton(
+      onPressed: () {
+        close(
+          context,
+          null,
+        ); //Esta funcion esta predefinida para cerrar la busqueda e ir hacia atras
+      },
+      icon: Icon(Icons.arrow_back),
+    );
   }
 
   @override
